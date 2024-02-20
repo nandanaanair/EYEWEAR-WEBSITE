9af47a6c00591125fcac943e593e62f60933f042
Nandana Nair <nandanaanair@gmail.com>
2024-02-20 18:07:26 +0530

    session

--- authenticate-user.php
+++ authenticate-user.php
@@ -1,19 +1,25 @@
 <?php
-// Start session
-session_start();
+session_start(); // Ensure session is started
 include "connect.php";
 
-// Function to check if user is logged in
 function isLoggedIn() {
     return isset($_SESSION['cust_email']);
 }
 
-// Function to restrict access to logged-in users
 function requireLogin() {
     if (!isLoggedIn()) {
-        // header("Location: login.php"); // Redirect to login page if user is not logged in
+        // Redirect to login page if user is not logged in
         echo "<script>window.location.href='index.html'</script>";
+        exit();
+    } elseif(basename($_SERVER['PHP_SELF']) !== 'home.php' && basename($_SERVER['PHP_SELF']) !== 'index.html') {
+        // Redirect to home page if user is logged in and not already on home.php or index.html
+        echo "<script>window.location.href='home.php'</script>";
         exit();
     }
 }
+
+
+// Call requireLogin() function
+requireLogin();
+
 ?>
