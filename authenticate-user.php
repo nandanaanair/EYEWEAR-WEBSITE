869448d5bfc882d3f54be0953914106be00f54df
Nandana Nair <nandanaanair@gmail.com>
2024-02-21 09:11:11 +0530

    redirection to home fixed

--- authenticate-user.php
+++ authenticate-user.php
@@ -11,11 +11,7 @@ function requireLogin() {
         // Redirect to login page if user is not logged in
         echo "<script>window.location.href='index.html'</script>";
         exit();
-    } elseif(basename($_SERVER['PHP_SELF']) !== 'home.php' && basename($_SERVER['PHP_SELF']) !== 'index.html') {
-        // Redirect to home page if user is logged in and not already on home.php or index.html
-        echo "<script>window.location.href='home.php'</script>";
-        exit();
-    }
+    } 
 }
 
 
